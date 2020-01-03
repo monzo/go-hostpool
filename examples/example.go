ee68b0e1381615f287731c7414fe74f323555d7b
Suhail Patel <me@suhailpatel.com>
2020-01-03 10:27:03 +0000

    Fix broken test

--- examples/example.go
+++ examples/example.go
@@ -7,7 +7,6 @@ import (
 func ExampleNewEpsilonGreedy() {
 	hp := hostpool.NewEpsilonGreedy([]string{"a", "b"}, 0, &hostpool.LinearEpsilonValueCalculator{})
 	hostResponse := hp.Get()
-	hostname := hostResponse.Host()
-	err := nil // (make a request with hostname)
-	hostResponse.Mark(err)
+	_ = hostResponse.Host()
+	hostResponse.Mark(nil)
 }
