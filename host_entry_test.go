ccec9f84024633c4fca7fc3dc615e41266cfe441
Kieran Gorman <kierangorman@monzo.com>
2020-07-15 17:24:59 +0100

    test: demonstrate missing bucket values

--- host_entry_test.go
+++ host_entry_test.go
@@ -82,6 +82,21 @@ func TestGetWeightAverageRespnseTime(t *testing.T) {
 			// 200 + 100 + 60 + 40 + 25
 			expected: float64(425),
 		},
+		{
+			name: "if one bucket has no entries, we take the previous value",
+			entry: &hostEntry{
+				epsilonCounts: []int64{2, 4, 5, 0, 5},
+				epsilonValues: []int64{2000, 1000, 500, 0, 125},
+				epsilonIndex:  4, // newest value in index 4
+			},
+
+			// (2000 / 2) * 0.2 + (1000 / 4) * 0.4 + (500 / 5) * 0.6 + <empty bucket>  + (125 / 5) * 1
+			//                                uses previous value 👉 + (500 / 5) * 0.8 +
+			// 1000 * 0.2 + 250 * 0.4 + 100 * 0.6 + (100 * 0.8 + 25 * 1
+			//                                             ☝️ weight still steps down
+			// 200 + 100 + 60 + 80 + 25
+			expected: float64(465),
+		},
 	}
 
 	for _, tc := range tcs {
